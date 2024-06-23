1d2acfa5b0354edf932fefdd481f937caa61f66f
The-New-Boi <akshatbharatverma@gmail.com>
2024-06-23 14:09:56 +0530

    Update BMI wala.py

--- BMI wala.py
+++ BMI wala.py
@@ -3,8 +3,8 @@ b = int(input("Enter your height in cm :"))
 BMI = a/((b**2)/100**2)
 print("your BMI is",BMI)
 if (BMI< 18.5):
-  print("kuposhan ka shikaar")
+  print("you are lower than normal BMI of a person or you r underweight")
 elif (BMI>18.5 and BMI<24.9):
-  print("sahi hai tu")
+  print("you r alright")
 else:
-  print("Duniya main gareebee teri vagah se hai")
+  print("you are above than normal BMI of a person or you r overweight")
